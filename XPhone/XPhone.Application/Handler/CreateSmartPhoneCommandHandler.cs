496a7d06db61ceada8297caa5cf986ef7caed2ef
Angelo Santos <gabrielange883@gmail.com>
2024-06-24 20:15:21 -0300

    createSmartPhoneCommandHandler

---
+++ XPhone/XPhone.Application/Handler/CreateSmartPhoneCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XPhone.Application.Command;
+using XPhone.Infrastructure.Repository;
+
+namespace XPhone.Application.Handler
+{
+    public class CreateSmartPhoneCommandHandler : ICommandHandler<CreateSmartPhoneCommand>
+    {
+        private readonly ISmartPhoneRepository _smartPhoneRepository;
+        private readonly IStockRepository _stockRepository;
+
+        public CreateSmartPhoneCommandHandler(ISmartPhoneRepository smartPhoneRepository, IStockRepository stockRepository)
+        {
+            _smartPhoneRepository = smartPhoneRepository;
+            _stockRepository = stockRepository;
+        }
+        public Task<Guid> HandlerAsync(CreateSmartPhoneCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
