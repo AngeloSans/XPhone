2769258485fb579568decf943134ae86936dd346
Angelo Santos <gabrielange883@gmail.com>
2024-08-19 20:57:17 -0300

    testing...

--- XPhone/XPhone.Domain/obj/Debug/net8.0/XPhone.Domain.AssemblyInfo.cs
+++ XPhone/XPhone.Domain/obj/Debug/net8.0/XPhone.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XPhone.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8cea862ae8645e750a3a88f88fc92c6aa09ff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("XPhone.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XPhone.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
